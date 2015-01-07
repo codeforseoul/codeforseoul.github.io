0c7db433b8d58e59b55b9669d23983e20be3f1be
thechunsik <thechunsik@gmail.com>
2015-01-07 16:56:28 +0900

    added events list

--- js/meetup.js
+++ js/meetup.js
@@ -1,37 +1,40 @@
 var Meetup = Meetup || {};
 
 Meetup.config = {
-	group_url: 'code-for-seoul',
-	group_id: '146177402',
-	api: '7c66136c4b2e242d241e289595e3546'
+  group_url: 'code-for-seoul',
+  group_id: '146177402',
+  api: '7e603761f462d75487f3e3021584a73'
 }
 
 Meetup.getEvents = function(options) {
-	var status = options && options.status || 'upcoming,past',
-			desc = options && options.desc || true;
+  var status = options && options.status || 'upcoming',
+      desc = options && options.desc || false;
 
-	var url = 'https://api.meetup.com/2/events?&key=' + this.config.api + '&sign=true&photo-host=public&desc=' + desc + '&group_urlname='+ this.config.group_url + '&page=10&status=' + status;
+  var url = 'https://api.meetup.com/2/events?&key=' + this.config.api + '&sign=true&photo-host=public&desc=' + desc + '&group_urlname='+ this.config.group_url + '&page=10&status=' + status;
 
-	$.ajax({
-		type: 'GET',
-		url: url,
-		async: false,
-		contentType: "application/json",
-		dataType: 'jsonp',
-		success: function(data) {
-			var events = [];
-			console.log(data);
+  $.ajax({
+    type: 'GET',
+    url: url,
+    async: false,
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(data) {
+      var events = [];
+      console.log(data);
 
-			$.each(data.results, function (key, val) {
-				var date = new Date(val.time);
-				convertedDate = (date.getMonth()+1) + '월 ' + date.getDate() + '일 ' + date.getHours() + ':' + date.getMinutes() + '시';
-				events.push("<li><h3 class='event_name'>" + val.name + "</h3>" + val.description + "<p>" + convertedDate + "</p>");
-			});
-			
-			$("<ul/>", {
-				"class": "event",
-				html: events.join("")
-			}).appendTo('.events');
-		}
-	});
+      $.each(data.results, function (key, val) {
+        var date = new Date(val.time);
+        convertedDate = (date.getMonth()+1) + '월 ' + date.getDate() + '일 ' + date.getHours() + ':' + date.getMinutes();
+        events.push("<div class='event'><h4 class='event_name'><a href='" + val.event_url + "' target='_blank'>" + val.name + "</a></h4><p><b>" + convertedDate + "</b></p>" + val.description.substring(0, 100) + "...<p>" + convertedDate + "</p></div>");
+      });
+      
+      $('.events').html(events.join(""));
+      // $("<ul/>", {
+      //   "class": "events",
+      //   html: events.join("")
+      // }).appendTo('.events');
+    }
+  });
 };
+
+Meetup.getEvents();
